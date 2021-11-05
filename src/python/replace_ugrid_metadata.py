341469ee16da67b7ea14db494b69e6bd4e053cf3
Wolfgang Hayek <wolfgang.hayek@niwa.co.nz>
2021-11-05 17:48:20 +1300

    Generate edge coords, tidy-up

--- src/python/replace_ugrid_metadata.py
+++ src/python/replace_ugrid_metadata.py
@@ -10,7 +10,8 @@ import netCDF4 as nc
 
 def get_fields(nc_file):
     """
-    Scan input netCDF file and identify all variables that are associated with model data fields
+    Scan input netCDF file and return a list of variables that are associated with model
+    data fields, requiring certain netCDF variable attributes for identification
     """
 
     fields = []
@@ -25,7 +26,8 @@ def get_fields(nc_file):
 
 def get_vertical_axes(nc_file):
     """
-    Scan input netCDF file and identify vertical axes
+    Scan input netCDF file and return a list of vertical axis variables, requiring specific
+    axis names
     """
 
     vertical_axes = []
@@ -40,7 +42,8 @@ def get_vertical_axes(nc_file):
 
 def get_time_vars(nc_file):
     """
-    Scan input netCDF file and identify time variables
+    Scan input netCDF file and return a list of time variables, requiring specific variable
+    names
     """
 
     time_vars = []
@@ -55,7 +58,8 @@ def get_time_vars(nc_file):
 
 def get_ugrid_mesh_description(nc_file):
     """
-    Collect UGRID mesh description from mesh file
+    Collect UGRID mesh description from mesh file and return a list of UGRID variables;
+    only one mesh description is allowed
     """
 
     # Look for UGRID mesh description dummy variable
@@ -71,9 +75,11 @@ def get_ugrid_mesh_description(nc_file):
 
     logging.info('Found UGRID mesh "%s"', mesh_names[0])
 
+    # Extract topology dummy var from the first (and only) list element
     ugrid_topology_var = nc_file.variables[mesh_names[0]]
 
-    # Collect expected UGRID location and connectivity variables
+    # Collect expected UGRID location and connectivity variable names from the dummy var
+    # attributes
     ugrid_varnames = set()
     ugrid_varnames.update(ugrid_topology_var.node_coordinates.split(" "))
     ugrid_varnames.update(ugrid_topology_var.face_coordinates.split(" "))
@@ -82,6 +88,7 @@ def get_ugrid_mesh_description(nc_file):
     ugrid_varnames.add(ugrid_topology_var.face_edge_connectivity)
     ugrid_varnames.add(ugrid_topology_var.face_face_connectivity)
 
+    # Assemble list of UGRID variables, including dummy var
     ugrid_vars = [ugrid_topology_var]
     for varname in ugrid_varnames:
         ugrid_vars.append(nc_file.variables[varname])
@@ -89,24 +96,19 @@ def get_ugrid_mesh_description(nc_file):
     return ugrid_vars
 
 
-def write_file(nc_file, ugrid_vars, vert_axes, time_vars, fields):
+def write_ugrid_vars(nc_file, ugrid_vars, output_mesh_name):
     """
-    Write netCDF output file
+    Write UGRID dimension and variables
     """
 
-    #
-    # UGRID
-    #
-
-    # Collect UGRID dimensions
+    # Collect UGRID dimensions from variables
     ugrid_dims = set()
     for ugrid_var in ugrid_vars:
         ugrid_dims.update(ugrid_var.get_dims())
 
     mesh_name = ugrid_vars[0].name
-    output_mesh_name = 'Mesh2d'
 
-    # Generate dictionary for translating dimension names
+    # Generate dictionary for translating between old and new UGRID dimension names
     ugrid_dim_translate = dict()
     for dim in ugrid_dims:
         ugrid_dim_translate[dim.name] = dim.name.replace(mesh_name, output_mesh_name)
@@ -130,74 +132,97 @@ def write_file(nc_file, ugrid_vars, vert_axes, time_vars, fields):
             else:
                 output_var.setncattr(att, att_value)
 
-    #
-    # Vertical axes
-    #
 
-    vert_dims = set()
-    for vert_axis in vert_axes:
-        vert_dims.update(vert_axis.get_dims())
+def generate_edge_coords(nc_file, output_mesh_name):
+    """
+    Generate edge coordinate field, if missing, by averaging over node locations
+    """
 
-    for dim in vert_dims:
+    if 'edge_coordinates' not in dir(nc_file.variables[output_mesh_name]):
+        logging.info('Generating edge coordinates...')
+        edge_node_conn = nc_file.variables[output_mesh_name + '_edge_nodes']
+        if 'start_index' in dir(edge_node_conn):
+            start_idx = int(edge_node_conn.start_index)
+        else:
+            start_idx = 0
+        node_lon = nc_file.variables[output_mesh_name + '_node_x']
+        node_lat = nc_file.variables[output_mesh_name + '_node_y']
+        edge_lon_var = nc_file.createVariable(output_mesh_name + '_edge_x', node_lon.datatype,
+                                              'n' + output_mesh_name + '_edge')
+        edge_lon_var[:] = 0.5*(node_lon[edge_node_conn[:,0] - start_idx] + \
+                               node_lon[edge_node_conn[:,1] - start_idx])
+        for att in node_lon.ncattrs():
+            edge_lon_var.setncattr(att, node_lon.getncattr(att).replace('node', 'edge'))
+        edge_lat_var = nc_file.createVariable(output_mesh_name + '_edge_y', node_lat.datatype,
+                                              'n' + output_mesh_name + '_edge')
+        edge_lat_var[:] = 0.5*(node_lat[edge_node_conn[:,0] - start_idx] + \
+                               node_lat[edge_node_conn[:,1] - start_idx])
+        for att in node_lat.ncattrs():
+            edge_lat_var.setncattr(att, node_lat.getncattr(att).replace('node', 'edge'))
+        nc_file.variables[output_mesh_name].setncattr('edge_coordinates', output_mesh_name + \
+                                                      '_edge_x ' + output_mesh_name + '_edge_y')
+
+
+def write_vars(nc_file, variables):
+    """
+    Write variable dimensions and data
+    """
+
+    dims = set()
+    for var in variables:
+        dims.update(var.get_dims())
+
+    for dim in dims:
         nc_file.createDimension(dim.name, dim.size)
 
-    for vert_axis in vert_axes:
-        output_axis = nc_file.createVariable(vert_axis.name, vert_axis.datatype,
-                                             vert_axis.dimensions)
-        output_axis[:] = vert_axis[:]
-        for att in vert_axis.ncattrs():
-            output_axis.setncattr(att, vert_axis.getncattr(att))
+    for var in variables:
+        out_var = nc_file.createVariable(var.name, var.datatype, var.dimensions)
+        out_var[:] = var[:]
+        for att in var.ncattrs():
+            out_var.setncattr(att, var.getncattr(att))
 
-    #
-    # Time axis
-    #
 
-    time_dims = set()
-    for time_var in time_vars:
-        time_dims.update(time_var.get_dims())
-
-    for dim in time_dims:
-        nc_file.createDimension(dim.name, dim.size)
-
-    for time_var in time_vars:
-        output_var = nc_file.createVariable(time_var.name, time_var.datatype, time_var.dimensions)
-        output_var[:] = time_var[:]
-        for att in time_var.ncattrs():
-            output_var.setncattr(att, time_var.getncattr(att))
-
-    #
-    # Fields
-    #
+def write_field_vars(nc_file, fields, output_mesh_name):
+    """
+    Write field dimensions and data
+    """
 
     # Collect dimensions from input file
     field_dims = set()
     for field in fields:
         field_dims.update(field.get_dims())
 
-    # Create new dimensions in output file
+    # Add any previously undefined dimensions - these should be field component dimensions,
+    # but there is no way to be sure, so report them to the user
     for dim in field_dims:
         if not any(x in dim.name for x in ('node', 'edge', 'face') +
                                           tuple(nc_file.dimensions.keys())):
-            print('Adding previously undefined dimension {}'.format(dim.name))
+            msg = 'Adding previously undefined dimension {}'.format(dim.name)
+            logging.info(msg)
             nc_file.createDimension(dim.name, dim.size)
 
     # Create new field variables in output file
     for field in fields:
+        # Substitute dimension names
         output_field_dims = []
         for dim in field.dimensions:
             if dim.endswith('face'):
-                output_field_dims.append('nMesh2d_face')
+                output_field_dims.append('n' + output_mesh_name + '_face')
             elif dim.endswith('edge'):
-                output_field_dims.append('nMesh2d_edge')
+                output_field_dims.append('n' + output_mesh_name + '_edge')
             elif dim.endswith('node'):
-                output_field_dims.append('nMesh2d_node')
+                output_field_dims.append('n' + output_mesh_name + '_node')
             else:
                 output_field_dims.append(dim)
+
         field_var = nc_file.createVariable(field.name, field.datatype, output_field_dims)
         field_var[:] = field[:]
+
         for att in field.ncattrs():
+            # Substitute old UGRID mesh name
             if att == 'mesh':
                 field_var.setncattr(att, output_mesh_name)
+            # Reconstruct coordinate names with new UGRID mesh name, if applicable
             elif att == 'coordinates':
                 coord_names = field.getncattr(att).split(' ')
                 new_coord_names = []
@@ -223,6 +248,7 @@ def get_args():
     parser.add_argument('outfile', help='Output UGRID filename')
     parser.add_argument('-f', '--force', action='store_true', help='Overwrite existing files')
     parser.add_argument('-q', '--quiet', action='store_true', help='Suppress output')
+    parser.add_argument('-m', '--meshname', default='Mesh2d', help='Output UGRID mesh name')
     return parser.parse_args()
 
 
@@ -242,16 +268,23 @@ def main():
     logging.info('LFRic mesh file: %s', args.meshfile)
     logging.info('Output file: %s', args.outfile)
 
+    # Load LFRic outut file and retrieve data fields, vertical axes, and time variables
     infile = nc.Dataset(args.infile, 'r')
     fields = get_fields(infile)
     vert_axes = get_vertical_axes(infile)
     time_vars = get_time_vars(infile)
 
+    # Load LFRic mesh file and retrieve UGRID variables
     meshfile = nc.Dataset(args.meshfile, 'r')
     ugrid_vars = get_ugrid_mesh_description(meshfile)
 
+    # Open output file and write variables and dimensions
     outfile = nc.Dataset(args.outfile, clobber=args.force, mode='w')
-    write_file(outfile, ugrid_vars, vert_axes, time_vars, fields)
+    write_ugrid_vars(outfile, ugrid_vars, args.meshname)
+    generate_edge_coords(outfile, args.meshname)
+    write_vars(outfile, vert_axes)
+    write_vars(outfile, time_vars)
+    write_field_vars(outfile, fields, args.meshname)
 
     outfile.close()
     meshfile.close()
